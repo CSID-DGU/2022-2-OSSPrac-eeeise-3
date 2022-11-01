0a41fef2f3cf2bc0b4ce53bfe663f0cbfcb31e2c
xhrisxavis23 <alzkdpf23@dgu.ac.kr>
2022-11-01 15:11:56 +0900

    Add Student Number

--- eeeise_hw3/main.py
+++ eeeise_hw3/main.py
@@ -10,8 +10,7 @@ def result():
    if request.method == 'POST':
       result = dict()
       result['Name'] = request.form.get('Name')
-
-      # 학번
+      result['Student Number'] = request.form.get('Student Number')
       # 성별
       # 학과
       # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
